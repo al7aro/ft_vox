42cfbd0eeb401a653b5a1a01de1fe1204c5a76e8
Alvaro <al7aro@gmail.com>
2026-01-28 01:09:20 +0100

    CHANGE: cube geometry now includes material_id and the UV mapping is a cubemap

--- src/engine/render/Cube.hpp
+++ src/engine/render/Cube.hpp
@@ -2,56 +2,60 @@
 
 namespace ft {
 
-    static inline std::vector<Vertex> cube_geometry(const glm::vec3& pos, const glm::vec4& color = glm::vec4(1.0))
+    static inline std::vector<Vertex> cube_geometry(const glm::vec3& pos, float material_id)
     {
+        float w = 1.0f / 4.0f;
+        float h = 1.0f / 3.0f;
+        glm::vec4 color(1.0);
         std::vector<Vertex> cube = {
-            // --- Front Face (Normal +Z) ---
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  0.0f,  1.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1.0f, 0.0f}, color, { 0.0f,  0.0f,  1.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  0.0f,  1.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  0.0f,  1.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {0.0f, 1.0f}, color, { 0.0f,  0.0f,  1.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  0.0f,  1.0f}},
+            // --- Front Face (+Z) (Row 1, Col 1) ---
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1*w, 1*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // BL
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {2*w, 1*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // BR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {2*w, 2*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {2*w, 2*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1*w, 2*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // TL
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1*w, 1*h}, color, { 0.0f,  0.0f,  1.0f}, {material_id}}, // BL
 
-            // --- Back Face (Normal -Z) ---
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  0.0f, -1.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {1.0f, 0.0f}, color, { 0.0f,  0.0f, -1.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  0.0f, -1.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  0.0f, -1.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {0.0f, 1.0f}, color, { 0.0f,  0.0f, -1.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  0.0f, -1.0f}},
+            // --- Back Face (-Z) (Row 1, Col 3) ---
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {3*w, 1*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // BL
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {4*w, 1*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // BR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {4*w, 2*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {4*w, 2*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {3*w, 2*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // TL
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {3*w, 1*h}, color, { 0.0f,  0.0f, -1.0f}, {material_id}}, // BL
 
-            // --- Left Face (Normal -X) ---
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, {-1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1.0f, 0.0f}, color, {-1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, {-1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, {-1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {0.0f, 1.0f}, color, {-1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, {-1.0f,  0.0f,  0.0f}},
+            // --- Left Face (-X) (Row 1, Col 0) ---
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0*w, 1*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // BL
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1*w, 1*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // BR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1*w, 2*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1*w, 2*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {0*w, 2*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // TL
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0*w, 1*h}, color, {-1.0f,  0.0f,  0.0f}, {material_id}}, // BL
 
-            // --- Right Face (Normal +X) ---
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {1.0f, 0.0f}, color, { 1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {0.0f, 1.0f}, color, { 1.0f,  0.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 1.0f,  0.0f,  0.0f}},
+            // --- Right Face (+X) (Row 1, Col 2) ---
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {2*w, 1*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // BL
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {3*w, 1*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // BR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {3*w, 2*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {3*w, 2*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {2*w, 2*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // TL
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {2*w, 1*h}, color, { 1.0f,  0.0f,  0.0f}, {material_id}}, // BL
 
-            // --- Top Face (Normal +Y) ---
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1.0f, 0.0f}, color, { 0.0f,  1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1.0f, 1.0f}, color, { 0.0f,  1.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {0.0f, 1.0f}, color, { 0.0f,  1.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {0.0f, 0.0f}, color, { 0.0f,  1.0f,  0.0f}},
+            // --- Top Face (+Y) (Row 2, Col 1) ---
+            // Note: Row 2 is the top row in UV space (OpenGL UVs start at bottom)
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1*w, 2*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // BL
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {2*w, 2*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // BR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {2*w, 3*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {2*w, 3*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z - 0.5f}, {1*w, 3*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // TL
+            Vertex{{pos.x - 0.5f, pos.y + 0.5f, pos.z + 0.5f}, {1*w, 2*h}, color, { 0.0f,  1.0f,  0.0f}, {material_id}}, // BL
 
-            // --- Bottom Face (Normal -Y) ---
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, { 0.0f, -1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {1.0f, 0.0f}, color, { 0.0f, -1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, { 0.0f, -1.0f,  0.0f}},
-            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1.0f, 1.0f}, color, { 0.0f, -1.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {0.0f, 1.0f}, color, { 0.0f, -1.0f,  0.0f}},
-            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {0.0f, 0.0f}, color, { 0.0f, -1.0f,  0.0f}},
+            // --- Bottom Face (-Y) (Row 0, Col 1) ---
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {1*w, 0*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // BL
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {2*w, 0*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // BR
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {2*w, 1*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x + 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {2*w, 1*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // TR
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z + 0.5f}, {1*w, 1*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // TL
+            Vertex{{pos.x - 0.5f, pos.y - 0.5f, pos.z - 0.5f}, {1*w, 0*h}, color, { 0.0f, -1.0f,  0.0f}, {material_id}}, // BL
         };
         return (cube);
     }
