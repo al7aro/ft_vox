07ab657905b66f0ec073ee9b16877e1488f22d1f
Alvaro <al7aro@gmail.com>
2026-01-28 19:09:22 +0100

    CHANGE: renderer JUST uploads state renderdata to the gpu

--- src/engine/render/VoxelBatchRenderer.cpp
+++ src/engine/render/VoxelBatchRenderer.cpp
@@ -3,25 +3,37 @@
 namespace ft {
 
     VoxelBatchRenderer::VoxelBatchRenderer()
-        : _vao(), _vbo(MAX_BATCH_SIZE), _current_shader(nullptr), _batch_cnt(0), _vertex_cnt(0)
+        : _vao(), _vbo(MAX_BATCH_SIZE), _batch_cnt(0), _vertex_cnt(0)
     {
         _vao.AddVBO(_vbo);
     }
 
     VoxelBatchRenderer::~VoxelBatchRenderer() {}
 
-    void VoxelBatchRenderer::Begin(std::shared_ptr<Shader> shader)
+    void VoxelBatchRenderer::Begin(const RenderData& data)
     {
-        _current_shader = shader;
+        if (data.shader)
+        {
+            data.shader->Bind();
+            data.shader->SetUniform("u_texture", 0);
+            for (int i = 0; i < data.materials.size(); i++)
+            {
+                data.shader->SetUniform("u_material[" + std::to_string(i) + "].Ka", data.materials[i]->Ka);
+                data.shader->SetUniform("u_material[" + std::to_string(i) + "].Kd", data.materials[i]->Kd);
+                data.shader->SetUniform("u_material[" + std::to_string(i) + "].Ks", data.materials[i]->Ks);
+                data.shader->SetUniform("u_material[" + std::to_string(i) + "].d", data.materials[i]->d);
+                data.shader->SetUniform("u_material[" + std::to_string(i) + "].diffuse_map_id", data.materials[i]->diffuse_map_id);
+            }
+        }
+        if (data.textures)
+            data.textures->BindTextureUnit(0);
         _batch_cnt = 0;
     }
 
-    void VoxelBatchRenderer::DrawVoxel(const glm::vec3 pos, glm::vec4 col)
+    void VoxelBatchRenderer::DrawVoxel(const glm::vec3 pos, int material_id)
     {
-        std::vector<Vertex> cube = cube_geometry(pos, col);
+        std::vector<Vertex> cube = cube_geometry(pos, material_id);
         unsigned int cube_vertices = cube.size();
-        size_t l1 = sizeof(Vertex) * (_vertex_cnt + cube_vertices);
-        size_t l2 = MAX_BATCH_SIZE;
         if (sizeof(Vertex) * (_vertex_cnt + cube_vertices) >= MAX_BATCH_SIZE)
             Flush();
         _vbo.AddData(cube);
@@ -36,8 +48,6 @@ namespace ft {
     void VoxelBatchRenderer::Flush()
     {
         _vao.Bind();
-        if (_current_shader)
-            _current_shader->Bind();
         glDrawArrays(GL_TRIANGLES, 0, _vertex_cnt);
         
         /* CLEAR */
@@ -50,5 +60,22 @@ namespace ft {
     {
         return (_batch_cnt);
     }
+
+    //unsigned int VoxelBatchRenderer::AddTextureArray(int width, int height, int depth)
+    //{
+    //    _texture_array = std::make_shared<Texture2DArray>(width, height, depth);
+    //    return (0);
+    //}
+
+    //unsigned int VoxelBatchRenderer::AddMaterial(const glm::vec4& color, const TextureSource& tex_src)
+    //{
+    //    glm::ivec2 texture_id(0, 0);
+    //    if (!_texture_array)
+    //        _texture_array = std::make_shared<Texture2DArray>(tex_src.width, tex_src.height);
+    //    else
+    //        texture_id.y = _texture_array->AddTexture(tex_src.data, tex_src.width, tex_src.height);
+    //    _materials.push_back(Material{color, texture_id});
+    //    return (_materials.size() - 1);
+    //}
 
 }
--- src/engine/render/VoxelBatchRenderer.hpp
+++ src/engine/render/VoxelBatchRenderer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include <glm/glm.hpp>
 
@@ -8,18 +9,28 @@
 #include "buffers/VAO.hpp"
 #include "buffers/VBO.hpp"
 #include "Shader.hpp"
-#include "Cube.hpp"
+#include "FPSCamera.hpp"
+#include "Material.hpp"
+#include "Texture2DArray.hpp"
+#include "../resource_manager/Cube.hpp"
+#include "../resource_manager/Resources.hpp"
 
 #define MAX_BATCH_SIZE sizeof(Vertex) * 36 * 1000 /* 100 CUBES */
 
 namespace ft {
+
+    struct RenderData
+    {
+        std::shared_ptr<Shader> shader;
+        std::shared_ptr<Texture2DArray> textures;
+        std::vector<std::shared_ptr<Material> > materials;
+    };
 
     class VoxelBatchRenderer
     {
     private:
         VAO _vao;
         VBO _vbo;
-        std::shared_ptr<Shader> _current_shader;
         unsigned int _batch_cnt;
         unsigned int _vertex_cnt;
 
@@ -27,12 +38,12 @@ namespace ft {
         VoxelBatchRenderer();
         ~VoxelBatchRenderer();
 
-        void Begin(std::shared_ptr<Shader> shader);
+        void Begin(const RenderData& data);
         void End();
 
         void Flush();
 
-        void DrawVoxel(const glm::vec3 pos, glm::vec4 col);
+        void DrawVoxel(const glm::vec3 pos, int material_id = -1);
 
         unsigned int GetBatchCount() const;
     };
